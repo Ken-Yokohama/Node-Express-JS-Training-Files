e559b9ce65f84f21952856133754a569d303f7f0
Ken Yokohama <kenlopezyokohama@gmail.com>
2021-10-21 09:38:18 +0800

    Add intro-to-Node

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var mySuperheroName = superheroes.random();
+
+console.log(supervillains.random());
+
+// ________ PRACTICE _____________
