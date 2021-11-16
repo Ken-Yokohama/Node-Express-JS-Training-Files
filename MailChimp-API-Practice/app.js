0f4b41b4f974409a6915eb7afd77f95fdb985edf
Ken Yokohama <kenlopezyokohama@gmail.com>
2021-11-16 09:47:41 +0800

    mailchimp api practice v2

---
+++ MailChimp-API-Practice/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+require("dotenv").config();
+
+const app = express();
+const port = 3000
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mailchimp.setConfig({
+  apiKey: "process.env.API_KEY",
+  server: "us5",
+});
+
+app.get("/", function(res, res) {
+  const run = async () => {
+    const response = await mailchimp.lists.getListMembersInfo("4f6e154e70");
+    console.log(response);
+    res.json(response);
+  };
+
+  run();
+});
+
+app.post("/",function(req,res){
+  const {email, status, fname, lname} = req.body;
+
+  const run = async () => {
+    const response = await mailchimp.lists.addListMember("4f6e154e70", {
+      email_address: email,
+      status: status,
+      merge_fields: {
+        FNAME: fname,
+        LNAME: lname
+      }
+
+    });
+    res.json(response);
+  };
+  run();
+
+});
+
+
+
+
+app.listen(port, function() {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
